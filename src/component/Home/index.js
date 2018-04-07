1f481ffc1b7fce5c29d81c4efbc35a41fd151df4
zhijianzhang <mrjzhang@tencent.com>
2018-04-07 21:32:04 +0800

    react+antd+less

---
+++ src/component/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.less'
+import {Button} from 'antd'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <Button type="primary">antd Button</Button>
+            <Button type="primary">antd Button</Button>
+            <Button type="primary">antd Button</Button>
+            <Button type="primary">antd Button</Button>
+        </div>
+
+    );
+}
+
+export default Home
